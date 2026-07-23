6dce02d8ef807c1050e1b861fee071900eab9766
clovenbradshaw-ctrl <clovenbradshaw@gmail.com>
2026-07-23 13:39:58 -0500

    Implement canonical ledger replay snapshot

--- packages/engine/index.js
+++ packages/engine/index.js
@@ -1,5 +1,5 @@
 export { projectReferents, surfacesIndicateSameReferent } from "./referents/index.js";
 export { CORE_SUBASSEMBLIES, assembleWatchmaker, defineSubassembly } from "./subassemblies/index.js";
 export { createState, applyCommand, appendEvents, replay, read } from "./replay/index.js";
-export { project } from "./projection/index.js";
+export { project, readingSnapshot } from "./projection/index.js";
 export { evaluate } from "./emergence/evaluate/index.js";
